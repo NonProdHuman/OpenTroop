6838ee3a365c398c493cb812294180ab7f6a3454
NonProdHuman <20018163+NonProdHuman@users.noreply.github.com>
2026-06-28 15:16:19 -0700

    chore: upgrade Cloudflare provider to v5 and update Terraform configuration syntax

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -1,30 +1,33 @@
-resource "cloudflare_record" "apex" {
+resource "cloudflare_dns_record" "apex" {
   count = var.cloudflare_enabled ? 1 : 0
 
   zone_id = var.cloudflare_zone_id
   name    = local.cloudflare_app_record
   type    = "CNAME"
-  value   = trimprefix(google_cloud_run_v2_service.web.uri, "https://")
+  content = trimprefix(google_cloud_run_v2_service.web.uri, "https://")
+  ttl     = 1
   proxied = true
 }
 
-resource "cloudflare_record" "wildcard" {
+resource "cloudflare_dns_record" "wildcard" {
   count = var.cloudflare_enabled ? 1 : 0
 
   zone_id = var.cloudflare_zone_id
   name    = local.cloudflare_wildcard_record
   type    = "CNAME"
-  value   = var.app_domain
+  content = var.app_domain
+  ttl     = 1
   proxied = true
 }
 
-resource "cloudflare_record" "api" {
+resource "cloudflare_dns_record" "api" {
   count = var.cloudflare_enabled ? 1 : 0
 
   zone_id = var.cloudflare_zone_id
   name    = local.cloudflare_api_record
   type    = "CNAME"
-  value   = trimprefix(google_cloud_run_v2_service.api.uri, "https://")
+  content = trimprefix(google_cloud_run_v2_service.api.uri, "https://")
+  ttl     = 1
   proxied = true
 }
 
@@ -32,39 +35,42 @@ resource "cloudflare_workers_script" "proxy" {
   count = var.cloudflare_enabled ? 1 : 0
 
   account_id         = var.cloudflare_account_id
-  name               = local.worker_name
+  script_name        = local.worker_name
   content            = file("${path.module}/worker/opentroop-proxy.js")
-  module             = true
+  main_module        = "opentroop-proxy.js"
   compatibility_date = "2024-04-03"
 
-  plain_text_binding {
-    name = "APP_DOMAIN"
-    text = var.app_domain
-  }
-
-  plain_text_binding {
-    name = "API_ORIGIN"
-    text = google_cloud_run_v2_service.api.uri
-  }
-
-  plain_text_binding {
-    name = "WEB_ORIGIN"
-    text = google_cloud_run_v2_service.web.uri
-  }
+  bindings = [
+    {
+      name = "APP_DOMAIN"
+      type = "plain_text"
+      text = var.app_domain
+    },
+    {
+      name = "API_ORIGIN"
+      type = "plain_text"
+      text = google_cloud_run_v2_service.api.uri
+    },
+    {
+      name = "WEB_ORIGIN"
+      type = "plain_text"
+      text = google_cloud_run_v2_service.web.uri
+    }
+  ]
 }
 
 resource "cloudflare_workers_route" "apex" {
   count = var.cloudflare_enabled ? 1 : 0
 
-  zone_id     = var.cloudflare_zone_id
-  pattern     = "${var.app_domain}/*"
-  script_name = cloudflare_workers_script.proxy[0].name
+  zone_id = var.cloudflare_zone_id
+  pattern = "${var.app_domain}/*"
+  script  = cloudflare_workers_script.proxy[0].script_name
 }
 
 resource "cloudflare_workers_route" "wildcard" {
   count = var.cloudflare_enabled ? 1 : 0
 
-  zone_id     = var.cloudflare_zone_id
-  pattern     = "*.${var.app_domain}/*"
-  script_name = cloudflare_workers_script.proxy[0].name
+  zone_id = var.cloudflare_zone_id
+  pattern = "*.${var.app_domain}/*"
+  script  = cloudflare_workers_script.proxy[0].script_name
 }
